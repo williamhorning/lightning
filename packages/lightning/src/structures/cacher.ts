ed8562b7932c131c057ae83bf4d0f5c30147947f
Jersey <wgyt735yt@gmail.com>
2025-05-18 13:54:14 -0400

    add docs for cacher

--- packages/lightning/src/structures/cacher.ts
+++ packages/lightning/src/structures/cacher.ts
@@ -1,8 +1,12 @@
+/** a class that wraps map to cache keys */
 export class cacher<k, v> {
+	/** the map used to internally store keys */
 	private map = new Map<k, { value: v; expiry: number }>();
 
+	/** create a cacher with a ttl (defaults to 30000) */
 	constructor(private ttl: number = 30000) {}
 
+	/** get a key from the map, returning undefined if expired or not found */
 	get(key: k): v | undefined {
 		const time = Temporal.Now.instant().epochMilliseconds;
 		const entry = this.map.get(key);
@@ -12,6 +16,7 @@ export class cacher<k, v> {
 		return undefined;
 	}
 
+	/** set a key in the map along with its expiry */
 	set(key: k, val: v, customTtl?: number): v {
 		const time = Temporal.Now.instant().epochMilliseconds;
 		this.map.set(key, {
