832357a3337dcc8c8865595baf40a8efa819cf12
Jersey <wgyt735yt@gmail.com>
2025-06-20 21:50:34 -0400

    fix revolt permissions and bump version

--- cli/main.go
+++ cli/main.go
@@ -20,7 +20,7 @@ func main() {
 	(&cli.Command{
 		Name:                  "lightning",
 		Usage:                 "extensible chatbot connecting communities",
-		Version:               "0.8.0-alpha.7",
+		Version:               "0.8.0-alpha.8",
 		DefaultCommand:        "help",
 		EnableShellCompletion: true,
 		Authors:               []any{"William Horning", "Lightning contributors"},
