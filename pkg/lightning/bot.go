ce537c1fa76a8c95d41dce2df58f4b2c5ce069a6
Jersey <wgyt735yt@gmail.com>
2025-09-11 13:50:16 -0400

    bump version

--- pkg/lightning/bot.go
+++ pkg/lightning/bot.go
@@ -7,7 +7,7 @@ import (
 )
 
 // VERSION is the version of the lightning bot framework.
-const VERSION = "0.8.0-beta.13"
+const VERSION = "0.8.0-rc.1"
 
 // BotOptions allows you to configure the prefix used by the bot for registered
 // commands, in addition to any platform specifics (like slash commands). If a
