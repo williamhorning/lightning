bde536f89708f0901baab5cf06c34107d540df24
Jersey <wgyt735yt@gmail.com>
2025-10-11 21:52:13 -0400

    don't block logging on webhook

--- internal/bridge/log.go
+++ internal/bridge/log.go
@@ -4,21 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
-
-type non2xxError struct {
-	code int
-}
-
-func (e *non2xxError) Error() string {
-	return "received non-2xx response: " + strconv.Itoa(e.code)
-}
 
 // SetupLogging creates a logger that deals with color and webhooks.
 func SetupLogging() *WebhookLogger {
@@ -39,34 +29,32 @@ type WebhookLogger struct {
 }
 
 func (l *WebhookLogger) Write(output []byte) (int, error) {
-	if l.URL == "" {
-		return len(output), nil
-	}
+	go func() {
+		if l.URL == "" {
+			return
+		}
 
-	data, err := json.Marshal(map[string]string{"content": "```ansi\n" + string(output) + "\n```"})
-	if err != nil {
-		return 0, fmt.Errorf("failed to marshal log: %w", err)
-	}
+		data, err := json.Marshal(map[string]string{"content": "```ansi\n" + string(output) + "\n```"})
+		if err != nil {
+			return
+		}
 
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, l.URL, bytes.NewBuffer(data))
-	if err != nil {
-		return 0, fmt.Errorf("failed to create log request: %w", err)
-	}
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, l.URL, bytes.NewBuffer(data))
+		if err != nil {
+			return
+		}
 
-	req.Header["Content-Type"] = []string{"application/json"}
+		req.Header["Content-Type"] = []string{"application/json"}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return 0, fmt.Errorf("failed to send log request: %w", err)
-	}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return
+		}
 
-	if err = resp.Body.Close(); err != nil {
-		return 0, fmt.Errorf("failed to close log response body: %w", err)
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return 0, &non2xxError{resp.StatusCode}
-	}
+		if err := resp.Body.Close(); err != nil {
+			return
+		}
+	}()
 
 	return len(output), nil
 }
